59bc66318fad4b6a0542a0b88d6c44de989a8078
“Samuel <SamScott.Sarantos@calbaptist.edu>
2023-09-15 12:28:17 -0700

    import React from 'react';

--- AuthPage.js
+++ AuthPage.js
@@ -14,7 +14,7 @@ function AuthPage() {
     }
   };
 
-  const handleRegister = async () => {
+    const handleRegister = async () => {
     try {
       await firebase.auth().createUserWithEmailAndPassword(email, password);
       // Handle successful registration
@@ -22,25 +22,53 @@ function AuthPage() {
       console.error("Error registering: ", error);
     }
   };
+  
+  const handleForgotPassword = async () => {
+    try {
+      await firebase.auth().sendPasswordResetEmail(email);
+      setError("Password reset link sent to your email.");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   return (
-    <div>
-      <input 
-        type="email" 
-        placeholder="Email" 
-        value={email} 
-        onChange={(e) => setEmail(e.target.value)} 
-      />
-      <input 
-        type="password" 
-        placeholder="Password" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)} 
-      />
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleRegister}>Register</button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+        <form className="mt-8 space-y-6">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <input 
+              type="email" 
+              placeholder="Email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button onClick={handleLogin} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Login
+            </button>
+            <button onClick={handleRegister} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
+
+  
 }
 
 export default AuthPage;
